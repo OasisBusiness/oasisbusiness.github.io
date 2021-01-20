35bbed62878253021d87e53ed1cd4a23d4dad9ea
sminy85 <smini857@naver.com>
2021-01-20 10:52:31 +0900

    ob

--- gatsby-node.js
+++ gatsby-node.js
@@ -71,7 +71,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === `MarkdownRemark`) {
-
     const value = createFilePath({ node, getNode })
     createNodeField({
       name: `slug`,
